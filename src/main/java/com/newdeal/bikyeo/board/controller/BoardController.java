d013fef870e069290e0ffe2c384ee8dfbe4638d1
mykyun <oka684@naver.com>
2019-01-10 17:11:13 +0900

    테이블(수정해야 할 것)

--- src/main/java/com/newdeal/bikyeo/board/controller/BoardController.java
+++ src/main/java/com/newdeal/bikyeo/board/controller/BoardController.java
@@ -1,9 +1,23 @@
 package com.newdeal.bikyeo.board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class BoardController {
 
+  /*
+   * 메소드명 : board()
+   * 작성일 : 19.01.10
+   * 작성자 : 김무균
+   * 설명 : 게시판페이지
+   */
   
+  @RequestMapping("/board")
+  public String board() {
+    
+    return "board.board";
+  }
   
   
   
