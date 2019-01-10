c67cac4d987700295b79ed49ed8e95a5976e451e
hohe12 <hohe123@naver.com>
2019-01-10 11:16:32 +0900

    대여소 뷰

--- src/main/java/com/newdeal/bikyeo/cycle/controller/CycleShareController.java
+++ src/main/java/com/newdeal/bikyeo/cycle/controller/CycleShareController.java
@@ -1,7 +1,22 @@
 package com.newdeal.bikyeo.cycle.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CycleShareController {
   
-  
+  /*
+   * 메소드명 : sharemain()
+   * 작성일 : 19.01.09
+   * 작성자 : 송기원
+   * 설명 : 인덱스페이지
+   */
+  @RequestMapping("/sharemain.do")
+  public String sharemain() {
+    //return "index.jsp";
+    return "share.main";
+  }
+
 
 }
