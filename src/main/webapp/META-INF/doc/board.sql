55b9cd9ded75bb5eef1ac06795bba16aa541ada2
염승민 <tmdals3021@gmail.com>
2019-01-24 15:19:12 +0900

    보드 더미 데이터 추가

---
+++ src/main/webapp/META-INF/doc/board.sql
@@ -0,0 +1,197 @@
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           1,
+           'bikyeo.masters@gmail.com',
+           '비켜는 무엇인가요?',
+           '<p>비켜는 누구나 , 언제나 , 어디서나 쉽고 편리하게 이용 할 수 있는 자전거 무인대여 시스템입니다.서울시의 교통체증, 대기오면 문제를 해결하고 건강한 사회와 시민들의 삶의 질을 높이고자 마련되었습니다.<br></p>',
+           '2019-01-24 11:21:43.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           2,
+           'bikyeo.masters@gmail.com',
+           '2018년 대여소 신규설치 및 추가 확대 계획은 있나요?',
+           '<p>○2018년 상반기까지 서울시 25개 자치구에 1,300개의 대여소가 설치 되었으며, 하반기에 추가구축 계획을 주진 중에 있습니다.</p><p><br></p><p>○비켜 대여소의 설치는 서울시와 해당 자치구의 대여소 후보지 선정과 설치 가능여부에 대하나 현장조사 및 이용시민 등의 의견을 거친 후 대여소 설치기준을 감안하여 설치완료 합니다.</p><p><br></p><p>○ 참고로 "대여소 설치 기준 요건"을 알려 드리겠습니다.</p><p>&nbsp; - 기존 서울공공자전거의 이용형태에 따른 대여소 상호연계성</p><p>&nbsp; - 대중교통 · 다중이용시설(관공서 및 공공시설 등) 주요 생활거점 지역성</p><p>&nbsp; - 보행안전 · 차로 등 교통환경, 주변상권 및 민원 등의 환경성&nbsp; &nbsp;</p><p>&nbsp; - 전기 인입이 가능한 시설(전신주,공중전화기,한전패드변압기 등에서 전원을 연결 할수 있어야 함)</p><p>&nbsp; - 설치공간은 보도폭이 4M이상 필요(장애인 점자블럭 저촉 제외)</p><p>&nbsp; - 자전거 배송 차량 진입로 확보 등</p>',
+           '2019-01-24 11:25:45.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           3,
+           'bikyeo.masters@gmail.com',
+           '추가요금부과 기준입니다',
+           '<p>○ 기본대여시간(60분) 초과시, 과금 발생&nbsp;</p><p>&nbsp; &nbsp; 기본료 시간 당 1천 원이고, 초과요금은 단위 시간 당 2천 원이 부과됩니다. 초과요금은 반납 시, 재결재를 요청합니다.</p>',
+           '2019-01-24 11:40:28.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           4,
+           'bikyeo.masters@gmail.com',
+           '비켜를 대여한 곳에만 반납해야 하나요?',
+           '<p>○ 아닙니다 . 자전거를 이용하신 후 가까운 대여소에 반납하시면 됩니다.</p><p>○ 가까운 대여소를 찾는 방법은 비켜 웹페이지 로그인 후 -&gt; 대여하기 -&gt; 대여소 선택 -&gt; 지도에서 ‘현재위치’ 확인 가능 합니다.</p><div><br></div>',
+           '2019-01-24 11:45:36.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           5,
+           'bikyeo.masters@gmail.com',
+           '비켜를 이용하고 싶은데 어떻게 해야 하나요?',
+           '<p>○자전거를 이용하는 방법은 웹페이지를 통해 가입 후 카카오페이를 이용해야 합니다.</p><p><br></p><p>1. 이용방법</p><p>‣ 회원가입-&gt;자전거대여-&gt;자전거반납 순으로 이용가능 하며, 각 순서는&nbsp;</p><p>다음의 내용을 통해서 확인 하실수 있습니다.&nbsp;</p><p><br></p><p>1-1 홈페이지를 통한 회원 가입&nbsp;</p><p>‣ 회원가입바로가기(&nbsp; http://localhost:8090/bikyeo/index.do )&nbsp;</p><p><br></p><p>1-2 이용권 결제&nbsp;</p><p>‣ 대여 시, 카카오페이로 결제합니다.</p><div><br></div>',
+           '2019-01-24 11:50:14.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           6,
+           'bikyeo.masters@gmail.com',
+           '자전거 반납은 어떻게 하나요?',
+           '웹페이지에서 로그인을 한 후 반납하기 버튼을 눌러서 반납을 진행합니다.',
+           '2019-01-24 12:05:39.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           7,
+           'bikyeo.masters@gmail.com',
+           '대여나 반납은 24시간 가능한가요?',
+           '<p>○ 비켜 대여소에 24시간 동안 대여와 반납이 가능합니다. 대여와 반납이 원할하지 않을 경우에 안내센터(010-6244-0703)으로 연락주시면 됩니다.</p><p>○ 안내센터 근무시간은 07:00 ~ 22:00까지 입니다. 안내센터 근무시간이외에는 다산콜센터로 연결되며 문제 발생 시 처리 어려울 수 있으니 참고 바랍니다.<br></p>',
+           '2019-01-24 12:06:37.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           8,
+           'bikyeo.masters@gmail.com',
+           '회원 아이디나 비밀번호를 모를 경우에는 어떻게 하나요?',
+           '<p>홈페이시 상의 ''아이디/비밀번호'' 찾기에서 개인인증을 통해 ''아이디/비밀번호''를 찾을 수 있습니다.</p>',
+           '2019-01-24 12:08:32.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           9,
+           'bikyeo.masters@gmail.com',
+           '회원인데 전화번호가 변경되었습니다. 어떻게 해야 하나요?',
+           '<p>-넌화번호가 변경되었습니다. 반드시 새로운 번호를 등록하셔야 합니다.</p><p>(홈페이지 로그인 -&gt; 마이페이지 -&gt; 정보수정 -&gt; 비밀번호 재입력 -&gt;전화번호 변경)</p>',
+           '2019-01-24 12:18:04.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           10,
+           'bikyeo.masters@gmail.com',
+           '요금 및 결제수단은 무엇인가요?',
+           '<p>○대여 시, 카카오페이로 결재하실 수 있습니다. 이용 금액 및 이용방법은 아래 페이지에서 확인 하실 수 있습니다.</p>',
+           '2019-01-24 12:23:26.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           11,
+           'bikyeo.masters@gmail.com',
+           '추가요금은 무엇이며 어떻게 결제하나요?',
+           '<p>&nbsp;- 지정된 시간을 초과하여 자전거를 이용하면 초과 이용시간에 따른 요금이 별도로 부과됩니다 .&nbsp;</p><p><br></p><p>&nbsp; · 추가요금은 카카오페이로 재결재 요청을 합니다.</p><p><br></p><p>&nbsp; · 추가요금이 부과되지 않도록 꼭 이용시간내에 반납하시고 재대여 해 주세요.</p><p><br></p><p>○ 추가요금은 단위 시간 당 2천 원입니다.</p><p><br></p><p><br></p><p>&nbsp;</p>',
+           '2019-01-24 12:26:32.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           12,
+           'bikyeo.masters@gmail.com',
+           '대여소는 무엇인가요?',
+           '<p>대여소는 비켜를 거치해두는 곳으로써 비켜 대여와 반납이 이루어지는 곳입니다.</p><p><br></p><p>대여소 기준은 지역주민 인구 및 이용편의성, 대중교통과 기존 대여소간의 연계성, 보행과 교통 등을 고려하여 설치됩니다.</p><p><br></p><p>특히, 보행안정을 위해 적정한 보행폭과 무선통신을 위한 전기 인입이 가능한 곳이어야 합니다.</p><p><br></p><p>대여소 설치는 위의 요소를 만족하더라도 자치구 및 지역주민 협조가 필요합니다.</p>',
+           '2019-01-24 12:37:48.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           13,
+           'bikyeo.masters@gmail.com',
+           '대여소의 목록을 알고 싶습니다.',
+           '<p>아래의 주소로 가시면 확인하실 수 있습니다.</p><p><span style="font-size: 18px;">http://localhost:8090/bikyeo/cycleshare/sharemain.do</span></p>',
+           '2019-01-24 12:41:17.0',
+           0);
+
+INSERT INTO `board`(`b_Num`,
+                    `m_Email`,
+                    `b_Subject`,
+                    `b_Content`,
+                    `b_Regdate`,
+                    b_hit)
+        VALUES (
+           14,
+           'bikyeo.masters@gmail.com',
+           '비켜의 브레이크의 앞뒤 브레이크는 어느 쪽인가요?',
+           '<p>○ 비켜의 앞브레이크는 왼쪽, 뒷브레이크는 오른쪽입니다.&nbsp;</p><p>&nbsp; - 이것은 재경부 산하 기술표준원 고시 제2009-0978(2009.12.30)에 의해 2010년도 기준으로 다음과 같이 변경이 되었습니다.&nbsp;</p><p>&nbsp; ※ 2010년도 이전 자전거는 왼쪽이 뒷브레이크 오른쪽이 앞브레이크로 되어있었습니다.&nbsp;</p><p>&nbsp; ※ 2010년도 이후 자전거는 왼쪽은 앞브레이크 오른쪽은 뒤브레이크로 변경이 되었습니다.</p><p><br></p><p>○ 비켜의 브레이크 사용요령 관련하여 앞브레이크는 정지역할이고 뒷브레이크는 속도조절 역할을 하기 때문에 실제 정지시에는 앞․뒤브레이크를 동시에 사용하여 정지하셔야 합니다.</p><p><br></p><p>&nbsp;※ 앞브레이크만 사용시 고속에서는 전복의 위험성이 있습니다.</p>',
+           '2019-01-24 14:18:30.0',
+           0);
+
+COMMIT;
